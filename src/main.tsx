d9787fda01695abf9e29b904db6be56dfb04b41e
IsacZetterstrom <isac.zetterstrom@hotmail.com>
2025-02-10 22:58:44 +0100

    Changed from react.strictmode to strictmode, importing it instead
    removed blankspace

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './Router/router.tsx'
+import { StrictMode } from 'react'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </StrictMode>
 );
